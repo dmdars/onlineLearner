d4e2793f4d241a943173b0e74250f8756836f865
dmdars <45941499+dmdars@users.noreply.github.com>
2021-02-06 23:17:14 +0100

    Revert "update servlet"
    This reverts commit 2312de1bf4cfbf5856b897090319ad36ba2898a0.

--- src/main/java/de/unidue/inf/is/view_mainServlet.java
+++ src/main/java/de/unidue/inf/is/view_mainServlet.java
@@ -1,19 +1,15 @@
 package de.unidue.inf.is;
-//import java.sql.*;
-//import java.util.*;
+
 import java.io.IOException;
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
-//import javax.lang.model.element.Name;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import de.unidue.inf.is.utils.DBUtil;
-import de.unidue.inf.is.domain.Kurs;
-import de.unidue.inf.is.domain.Benutzer;
-import de.unidue.inf.is.stores.KursStore;
+
+import de.unidue.inf.is.domain.User;
 
 
 
@@ -23,17 +19,24 @@ import de.unidue.inf.is.stores.KursStore;
 public final class view_mainServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-    public static Benutzer user = new Benutzer();
-    public static String name = user.getName();
 
-    private static List<Kurs> list = KursStore.getallKurs();
-    private static List<Benutzer> list2 = KursStore.getUserKurs();
+    private static List<User> userList = new ArrayList<>();
+
     // Just prepare static data to display on screen
+    static {
+        userList.add(new User("Bill", "Gates"));
+        userList.add(new User("Steve", "Jobs"));
+        userList.add(new User("Larry", "Page"));
+        userList.add(new User("Sergey", "Brin"));
+        userList.add(new User("Larry", "Ellison"));
+    }
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Put the user list in request and let freemarker paint it.
-        request.setAttribute("ukurs", list2);
-        request.setAttribute("kurs", list);
+        request.setAttribute("users", userList);
+
         request.getRequestDispatcher("/view_main.ftl").forward(request, response);
     }
 
@@ -42,17 +45,17 @@ public final class view_mainServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
                     IOException {
 
-//        String freiePlaetze = request.getParameter("freiePlaetze");
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
 
-          
-//        if(name != null && name != null && name != null)
-//        {
-//        String ersteller = request.getParameter("ersteller");
-//        request.getSession().setAttribute("name", name);
+        if (null != firstname && null != lastname && !firstname.isEmpty() && !lastname.isEmpty()) {
+
+            synchronized (userList) {
+                userList.add(new User(firstname, lastname));
+            }
+
+        }
+
         doGet(request, response);
-        String bname = request.getParameter("bname");
-        name = request.getParameter("kname");
-//        user.setName(kname);
-        user.setBname(bname);
     }
 }
